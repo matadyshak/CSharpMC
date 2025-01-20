3dd5a7dcd5a265d20e67477a56197b76c980ffac
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-01-19 22:22:09 -0600

    My second project at end of lesson

---
+++ Instructor/MySecondProject/GreetingApp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+// Welcome User To App
+Console.WriteLine("Welcome to the Greeting Application");
+Console.WriteLine("This application was built by Tim Corey");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine();
+
+// Ask for First Name
+Console.Write("What is your first name: ");
+string firstName;
+firstName = Console.ReadLine();
+
+// Greet user by name
+Console.WriteLine();
+Console.WriteLine("Hello " + firstName);
+
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
---
+++ Student/MySecondProject/GreetingApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Welcome user to app
+Console.WriteLine("Welcome to the Greeting Application.");
+Console.WriteLine("This application was built by Michael Anthony Tadyshak.");
+Console.WriteLine("-------------------------------------------------------");
+Console.WriteLine();
+
+// Ask for first name
+Console.Write("What is your first name: ");
+string firstName;
+firstName = Console.ReadLine();
+
+// Greet user by name
+Console.WriteLine();
+//Console.WriteLine("Greetings, {0}!  The Lord be with You!", firstName); 
+Console.WriteLine("Hello " + firstName);
+
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
