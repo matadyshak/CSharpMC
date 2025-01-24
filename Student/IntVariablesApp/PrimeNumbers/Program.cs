005b079699fe3ed46ad391b854c6b819e998a74b
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-01-23 23:00:40 -0600

    Added Factorial project.  Added project to display the longest sequences of no prime numbers.  Added elementary data types min, max, size using $ notation.  Added Fibonacci sequence.

--- Student/IntVariablesApp/PrimeNumbers/Program.cs
+++ Student/IntVariablesApp/PrimeNumbers/Program.cs
@@ -1,26 +1,26 @@
-﻿amespace PrimeNumbers
+﻿namespace PrimeNumbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             bool bPrime = true;
-            uint numerator = 0;
-            uint denominator = 0;
+            int numerator = 0;
+            int denominator = 0;
+            int prevPrime = 2;
+            int currPrime = 3;
+            int maxBetweenPrimes = 1;
+            int primeCount = 0;
 
-            //First prime numbers: 
-            uint[] primeNumbers = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23];
+            Console.WriteLine("This will display all prime numbers from 2 to N.");
+            Console.Write("Enter a positive integer for N: ");
+            int maxNumerator = int.Parse(Console.ReadLine());
+            //Console.WriteLine("2");
 
-            foreach (uint primeNumber in primeNumbers)
+            for (numerator = 3; numerator <= maxNumerator; numerator += 2)
             {
-                Console.WriteLine(primeNumber.ToString());
-            }
-
-            //     for (uint numerator = 29; numerator < uint.MaxValue; numerator += 2)
-            for (numerator = 29; numerator < 1000000u; numerator += 2)
-            {
-                uint maxDenominator = numerator / 2 + 2;
-                for (denominator = 2; denominator <= maxDenominator; denominator++)
+                int maxDenominator = (int) Math.Sqrt(numerator) + 1;
+                for (denominator = 3; denominator <= maxDenominator; denominator++)
                 {
                     bPrime = true;
                     if (numerator % denominator == 0)
@@ -32,47 +32,26 @@
 
                 if (bPrime == true)
                 {
-                    Console.WriteLine(numerator.ToString());
-                }
-                else
-                {
-                    Console.WriteLine($"{numerator} / {denominator} = {numerator / denominator}");
+                    primeCount++;
+                    prevPrime = currPrime;
+                    currPrime = numerator;
+                    if(( currPrime - prevPrime - 1) > maxBetweenPrimes)
+                    {
+                        maxBetweenPrimes  = currPrime - prevPrime - 1; 
+                        Console.WriteLine($"Previous Prime# {prevPrime} Current Prime# {currPrime} Stretch of No Prime#'s: {currPrime - prevPrime - 1} Total Prime#'s {primeCount}");
+                    }
+                    //Console.WriteLine(numerator.ToString());
                 }
             }
         }
     }
 }
 
+/*
+The expression (int) Math.Sqrt(numerator) in C# will result in the whole part of the number, 
+not a rounded number. This is because the (int) cast truncates the decimal part of the result,
+effectively giving you the integer part of the square root.
 
-    static void Main()
-{
-    Console.Write("Enter a positive integer: ");
-    int number = int.Parse(Console.ReadLine());
-
-    bool isPrime = true;
-
-    if (number <= 1)
-    {
-        isPrime = false;
-    }
-    else
-    {
-        for (int i = 2; i <= Math.Sqrt(number); i++)
-        {
-            if (number % i == 0)
-            {
-                isPrime = false;
-                break;
-            }
-        }
-    }
-
-    if (isPrime)
-    {
-        Console.WriteLine($"{number} is a prime number.");
-    }
-    else
-    {
-        Console.WriteLine($"{number} is not a prime number.");
-    }
-}
+If you need the result to be rounded to the nearest integer, you can use 
+Math.Round(Math.Sqrt(numerator)) instead.
+*/
