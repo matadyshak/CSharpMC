db42928325b34a8105453f807b2e7ff35f85d921
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-01-30 22:51:58 -0600

    Completed DateOnly exercise

---
+++ Instructor/DateOnlyVariablesApp/DateOnlyVariables/Program.cs
@@ -0,0 +1,11 @@
+﻿
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse("6/11/1998");
+
+Console.WriteLine(birthday.ToString("MMMM dd, yyyy"));
+
+
+Console.WriteLine($"Today full format: {today}");
+Console.WriteLine($"Today just date: {today.Date}");
+Console.WriteLine($"Birthday full format: {birthday}");
---
+++ Student/DateOnlyVariablesApp/DateOnlyVariables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace DateOnlyVariables
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DateTime todayDT = DateTime.Now;
+            Console.WriteLine(String.Format("{0:MM-dd-yy hh:mm:ss tt}", todayDT));
+            // Still a date and time with time set to 12:00:00 AM
+            Console.WriteLine(String.Format("{0:MM-dd-yy hh:mm:ss tt}", todayDT.Date)); 
+
+            DateOnly today = DateOnly.FromDateTime(todayDT);
+            //Console.WriteLine(String.Format("{0:MM-dd-yy hh:mm:ss tt}", today));
+            Console.WriteLine(String.Format("{0:MM-dd-yy}", today));
+
+            DateOnly tomorrow = today.AddDays(1);
+            Console.WriteLine(String.Format("{0:MM-dd-yy}", tomorrow));
+
+            DateOnly yesterday = today.AddDays(-1);
+            Console.WriteLine(String.Format("{0:MM-dd-yy}", yesterday));
+
+            DateOnly birthday = new DateOnly(1962, 10, 23);
+            //Console.WriteLine(String.Format("{0:MM-dd-yy hh:mm:ss tt}", birthday));
+            Console.WriteLine(String.Format("{0:MM-dd-yy}", birthday));
+
+            string dateString = "23 October 1962";
+            DateOnly date = DateOnly.ParseExact(dateString, "dd MMMM yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.ToString("MMMM dd, yyyy"));
+
+            // Not supported
+            // DateOnly dt1 = new DateOnly(ticks);
+        }
+    }
+}
