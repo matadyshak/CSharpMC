f29c2078105478ad1e117fa6ef7ed2bcb3408ca9
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-04 21:30:43 -0600

    Completed homework assignment for practicing if and switch statements.

---
+++ Student/HomeworkIfSwitchApp/HomeworkIfSwitch/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeworkIfSwitch
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine().ToLower();
+
+            if (name == "tim" || name == "timothy")
+            {
+                Console.WriteLine("Welcome, Professor");
+            }
+            else
+            {
+                Console.WriteLine("Welcome, Student");
+            }
+        }
+    }
+}
---
+++ Student/HomeworkIfSwitchApp/HomeworkSwitch/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeworkSwitch
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine().ToLower();
+
+            switch(name)
+            {
+                case "tim":
+                case "timothy":
+                    Console.WriteLine("Welcome, Professor");
+                    break;
+                default:
+                    Console.WriteLine("Welcome, Student");
+                    break;
+            }
+            return;
+        }
+    }
+}
