488513354b804f4892de3ec017ed278b70dc4c8b
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-06 22:45:48 -0600

    Completed HomeworkDoLoopApp

---
+++ Student/HomeworkDoLoopApp/HomeworkDoLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿string firstName = "";
+
+do
+{
+    Console.Write("Enter your first name (or type exit to end program)? ");
+    firstName = Console.ReadLine();
+
+    if (firstName.Length == 0)
+    {
+        continue;
+    }
+
+    if (firstName.ToLower() == "exit")
+    {
+        break;
+    }
+
+    if (firstName.ToLower() == "tim")
+    {
+        Console.WriteLine($"Hello Professor {firstName}!");
+    }
+    else
+    {
+        Console.WriteLine($"Hello student {firstName}!");
+    }
+
+} while (true);
