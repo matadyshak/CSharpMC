f80393500313f2c5cf744d29c447a95313bd0187
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-06 22:53:00 -0600

    Starting ArraysApp lesson.

---
+++ Instructor/ArraysApp/Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+// 0-based counting - 0,1,2,3,4
+// 1-based counting - 1,2,3,4,5
+
+//string[] firstNames = new string[5];
+
+//firstNames[0] = "Tim";
+//firstNames[1] = "Sue";
+//firstNames[2] = "Bob";
+//firstNames[4] = "Jane";
+
+//Console.WriteLine($"The firstNames are {firstNames[0]}, {firstNames[1]}, {firstNames[2]}, {firstNames[4]}");
+
+//firstNames[0] = "Timothy";
+
+//Console.WriteLine(firstNames[0]);
+
+//firstNames[5] = "Robert";
+
+// single quote identifies a char
+// double quotes identifies a string
+string data = "Tim,Sue,Bob,Jane,Frank";
+string[] firstNames = data.Split(',');
+
+Console.WriteLine(firstNames[firstNames.Length - 1]);
+
+Console.WriteLine(firstNames.Length);
+
+string[] lastNames = new string[] { "Corey", "Smith", "Jones" };
+
+int[] ages = new int[] { 2, 3, 4 };
