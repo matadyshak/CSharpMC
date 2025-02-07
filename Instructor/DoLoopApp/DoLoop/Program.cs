4c2b6c4f775f0ace8ea60acbcdf635c975f19579
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-06 22:24:29 -0600

    Complted practice DoLoopApp with employee rating system.

---
+++ Instructor/DoLoopApp/DoLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿
+bool isValidAge;
+int age;
+//int testNumber = 0;
+
+//do
+//{
+//    Console.WriteLine(testNumber);
+//    testNumber += 3;
+//} while (testNumber < 10);
+
+do
+{
+    Console.Write("What is your age: ");
+    string ageText = Console.ReadLine();
+
+    isValidAge = int.TryParse(ageText, out age);
+
+    if (isValidAge == false)
+    {
+        Console.WriteLine("That was an invalid age.");
+    }
+} while (isValidAge == false);
+
+Console.WriteLine($"Your age is {age}");
+
+//do
+//{
+//    // Run the code at least once
+//} while (true);
+
+//while (true)
+//{
+//    // Runs the code 0 or more times
+//}
---
+++ Student/DoLoopApp/DoLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+string[] employeeNames = { "John", "Mary", "Sue", "Bob", "Alice" };
+
+int[,] employees = {{ 4, 3, 4 }, { 3, 4, 5 }, { 3, 3, 3 }, { 5, 2, 1 }, { 3, 4, 4 }};
+
+int employeeCount = 5;
+int employee = 0;
+int metricCount = 3;
+int metric = 0;
+
+double[] overallScores = { 0.0d, 0.0d, 0.0d, 0.0d, 0.0d };  //Averages of RAD, NAS, INV for 5 employees
+
+do
+{
+    metric = 0;
+    do
+    {
+        overallScores[employee] += employees[employee, metric];
+        metric++;
+    } while (metric < metricCount);
+
+    overallScores[employee] /= metricCount;
+    employee++;
+} while (employee < employeeCount);
+
+employee = 0;
+do
+{
+    Console.WriteLine($"Employee: {employeeNames[employee]} has an overall score of: {overallScores[employee]:F2}");
+    employee++;
+} while (employee < employeeCount) ;
+
+
+
+
+
+
+
