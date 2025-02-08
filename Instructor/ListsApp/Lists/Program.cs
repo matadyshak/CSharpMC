251bb7ebccdd0815cbb2502ad28118d601908f35
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-08 12:14:28 -0600

    Completed ListsApp.

---
+++ Instructor/ListsApp/Lists/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// string[] firstNames = new string[5];
+List<string> firstNames = new List<string>();
+
+firstNames.Add("Tim");
+firstNames.Add("Sue");
+firstNames.Add("Bob");
+firstNames.Add("Jane");
+firstNames.Add("Frank");
+
+Console.WriteLine(firstNames[firstNames.Count - 1]);
+
+List<int> ages = new List<int>();
+ages.Add(1);
+ages.Add(2);
+ages.Add(3);
+
+// List<T> - generic
+
+string data = "Corey,Smith,Jones";
+List<string> lastNames = data.Split(',').ToList();
+lastNames.Add("Franklin");
---
+++ Student/ListsApp/Lists/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+using System.Reflection.Metadata;
+
+List <string> bowlers = new List<string>();
+bowlers.Add("Squiggy");
+bowlers.Add("Pal");
+bowlers.Add("Fred");
+bowlers.Add("Myra");
+bowlers.Add("Michael");
+
+List <int> scores = new List<int>();
+scores.Add(100);
+scores.Add(120);
+scores.Add(150);
+scores.Add(175);
+scores.Add(200);
+
+scores.Add(120);
+scores.Add(140);
+scores.Add(160);
+scores.Add(180);
+scores.Add(200);
+
+scores.Add(150);
+scores.Add(116);
+scores.Add(170);
+scores.Add(180);
+scores.Add(200);
+
+for (int game = 0; game < 3; game++)
+{
+    for (int player = 0; player < bowlers.Count; player++)
+    {
+        Console.WriteLine($"{bowlers[player]} scored {scores[game*bowlers.Count+player]}");
+    }
+    Console.WriteLine("\n");
+}
+
+
+string namesString = "Mike,Myra,Matt,Joe,Kris,Dan,Sarah";
+List<string> names = namesString.Split(',').ToList();
+names.Add("Eilif");
+foreach(string name in names)
+{
+    Console.WriteLine(name);
+}   
