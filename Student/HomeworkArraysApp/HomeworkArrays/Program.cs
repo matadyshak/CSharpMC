088d37e8384e65bc4e45ce37aac97a719507458e
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-08 09:26:07 -0600

    Completed HomeworkArraysApp.  Included commented out code for string.Contains method, Console.ReadKey() and ConsoleKey.Escape for ending a program via the escape key.

---
+++ Student/HomeworkArraysApp/HomeworkArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+string[] names = ["Peter", "James", "John"];
+string input = "";
+int index = 0;
+bool isValid = false;
+
+do
+{
+    Console.Write("Pick a name by entering an array index (0 | 1 | 2): ");
+    input = Console.ReadLine();
+    isValid = int.TryParse(input, out index) && index >= 0 && index < names.Length;
+    if(!isValid)
+    {
+        Console.WriteLine($"Invalid input: \'{input}\'. Please try again.");
+    }
+} while (!isValid);
+
+Console.Write($"You selected the name: \'{names[index]}\'");
+
+
+//if (names.Contains(name))
+//    {
+//        Console.WriteLine("Name found");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Name not found");
+//    }
+//} while (Console.ReadKey().Key != ConsoleKey.Escape);
+//Console.WriteLine();
