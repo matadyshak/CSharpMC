ef3136d5e6446381e8db9da68c2b752610910624
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-08 19:24:54 -0600

    Completed HomeworkListsApp.  Found issue trying Console.Readkey() where the first char gets lost when printing "Students in the class".

---
+++ Student/HomeworkListsApp/HomeworkLists/Program.cs
@@ -0,0 +1,31 @@
+﻿
+List <string> students = new List<string>();
+
+do
+{
+    Console.Write("\nEnter student's last name (or type 'exit' to finish): ");
+    string studentName = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(studentName))
+    {
+        Console.WriteLine("Invalid input. Please enter a valid name.");
+        continue;
+    }
+
+    if (studentName.ToLower() == "exit")
+    {
+        break;
+    }
+
+    students.Add(studentName);
+} while (true);
+
+    // This caused the first char of "Students in the class: " to be consumed
+    //} while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+Console.WriteLine("Students in the class: ");
+
+foreach(string student in students)
+{
+    Console.WriteLine(student);
+}
