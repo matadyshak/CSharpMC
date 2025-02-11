fa9ab79120086b049fc4c1fc13cc9481f0689fa9
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-10 23:20:02 -0600

    Completed HomeworkForeachLoopsApp

---
+++ Student/HomeworkForeachLoopsApp/HomeworkForeachLoops/Program.cs
@@ -0,0 +1,42 @@
+﻿List<string> firstNames = new List<string>();
+bool isValid = false;
+bool isDone = false;
+string? input = null;
+
+while (!isDone)
+{
+    isValid = false;
+    while (!isValid)
+    {
+        Console.Write("Enter a first name: ");
+        input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            isValid = false;
+            Console.WriteLine($"Name entered: \'{input}\' is invalid.");
+        }
+        else if (input.ToLower() == "exit")
+        {
+            isDone = true;
+            break;
+        }
+        else
+        {
+            isValid = true;
+            try
+            {
+                firstNames.Add(input);
+            }
+            catch
+            {
+                Console.WriteLine($"Failed to add name: \'{input}\' to the list");
+            }
+        }
+    } // while(!valid)
+} // while(!isDone)
+
+Console.WriteLine("\nThe names entered are: ");
+foreach (string firstName in firstNames)
+{
+    Console.WriteLine(firstName);
+}
