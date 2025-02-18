66a1c69b7a61f401d6b7dc526a7a2baf3a6191af
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-17 21:37:30 -0600

    Completed UsingBreakpointsDemoApp

---
+++ Instructor/UsingBreakpointsDemoApp/ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"The value of i is { i }");
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine($"The value of j is { j }");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student/UsingBreakpointsDemoApp/ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isValid;
+            string numberText;
+            int number;
+            int i;
+            int total = 0;
+
+            do
+            {
+                Console.Write("Enter an integer: ");
+                numberText = Console.ReadLine().Trim();
+                isValid = int.TryParse(numberText, out number);
+            } while (!isValid);
+
+            for (i = 0; i < 10; i++)
+            {
+                total += (5 * number);
+                Console.WriteLine(total);
+            }
+
+            return;
+        }
+    }
+}
