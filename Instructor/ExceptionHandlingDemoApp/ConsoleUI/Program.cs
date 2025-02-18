53cc1f7768af36352f5118333ee9c7845424d5b4
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-17 22:22:54 -0600

    Added instructor's ExceptionHandlingDemo

---
+++ Instructor/ExceptionHandlingDemoApp/ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BadCall();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an exception thrown in our app");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void BadCall()
+        {
+            int[] ages = new int[] { 1, 3, 5 };
+
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(ages[i]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("We had an error");
+                    Console.WriteLine(ex.Message);
+                    throw new Exception("There was an error handling our array", ex);
+                }
+            }
+        }
+    }
+}
