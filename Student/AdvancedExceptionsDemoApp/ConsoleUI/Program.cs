25d9e19da4ee446968c24fea9b62668f82d6a498
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-18 22:51:05 -0600

    Completed AdvancedExceptionsDemoApp

---
+++ Student/AdvancedExceptionsDemoApp/ConsoleUI/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string name = "";
+
+            try
+            {
+                Console.Write("What is your name: ");
+                name = Console.ReadLine().ToLower();
+                if (name == "michael")
+                {
+                    DivideByZeroMethod();
+                }
+                else if (name == "mike")
+                {
+                    IndexOutOfRangeMethod();
+                }
+                else if (name == "mickey")
+                {
+                    NotImplementedMethod();
+                }
+                else if (name == "tgd")
+                {
+                    InsufficientMemoryException();
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("A divide by zero exception has occurred in Main()");
+                Console.WriteLine(ex.Message);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("An index out of range exception has occurred in Main().");
+                Console.WriteLine(ex.Message);
+            }
+            catch (NotImplementedException ex)
+            {
+                Console.WriteLine("You did not implement code for a method.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex) when (name.ToLower() == "tgd")
+            {
+                Console.WriteLine("Michael, An exception has occurred in Main()");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block always runs.");
+            }
+            Console.WriteLine("End of Main program.");
+        }
+
+        public static void DivideByZeroMethod()
+        {
+            int constant = 25;
+            int quotient = 0;
+            int i;
+
+            for (i=-5; i<5; i++)
+            {
+                try
+                {
+                    quotient = constant / i;
+                    Console.WriteLine($"quotient: {quotient}, i: {i}");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("A divide by zero exception has occurred in DivideByZeroMethod()");
+                    Console.WriteLine(ex.Message);
+                    throw new DivideByZeroException();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An exception has occurred in DivideByZeroMethod().");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return;
+        }
+
+        public static void NotImplementedMethod()
+        {
+            throw new NotImplementedException();
+        }
+        public static void IndexOutOfRangeMethod()
+        {
+            int[] Array = { 5, 6, 7 };
+
+            try
+            {
+                for (int i = 0; i<=Array.Length; i++)
+                {
+                    Console.WriteLine($"Array[i] = {Array[i]}");
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception occurred in IndexOutOfRangeMethod.");
+                Console.WriteLine(ex.Message);
+                throw new IndexOutOfRangeException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred in IndexOutOfRangeMethod.");
+                Console.WriteLine(ex.Message);
+            }
+            return;
+        }
+
+        public static void InsufficientMemoryException()
+        {
+            throw new InsufficientMemoryException();
+        }
+    }
+}
+
--- Student/ExceptionHandlingDemoApp/ConsoleUI/Program.cs
+++ Student/ExceptionHandlingDemoApp/ConsoleUI/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            try
+            {
+                DivideByZeroMethod();
+                Console.WriteLine("After call to DivideByZeroMethod().");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("A divide by zero exception has occurred in Main()");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception has occurred in Main()");
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("End of Main program.");
+        }
+
+        public static void DivideByZeroMethod()
+        {
+            int constant = 25;
+            int quotient = 0;
+            int i;
+
+            for (i=-5; i<5; i++)
+            {
+                try
+                {
+                    quotient = constant / i;
+                    Console.WriteLine($"quotient: {quotient}, i: {i}");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("A divide by zero exception has occurred in DivideByZeroMethod()");
+                    Console.WriteLine(ex.Message);
+                    throw new DivideByZeroException();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An exception has occurred in DivideByZeroMethod().");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return;
         }
     }
 }
