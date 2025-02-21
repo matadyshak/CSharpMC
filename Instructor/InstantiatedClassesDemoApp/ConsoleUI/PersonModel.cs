a7073e7d22d2983e0c91e0fddfb1bae41f6541ed
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-21 10:14:37 -0600

    Instantiation class for instructor

---
+++ Instructor/InstantiatedClassesDemoApp/ConsoleUI/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    // Blueprint
+    public class PersonModel
+    {
+        //public string firstName;
+        //public string lastName;
+        //public string emailAddress; 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public bool HasBeenGreeted { get; set; }
+    }
+}
