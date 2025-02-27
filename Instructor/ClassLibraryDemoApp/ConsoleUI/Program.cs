2c5e96759395818978b358ae00f1c26c6090da33
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-02-27 10:15:37 -0600

    Starting ClassLibraryDemoApp.

---
+++ Instructor/ClassLibraryDemoApp/ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+            
+            Console.ReadLine();
+        }
+    }
+}
