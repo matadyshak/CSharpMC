eaf950667ed37fa6ab620056312071970165fb51
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-03-02 17:01:02 -0600

    ClassLibraryDemoAPP - include using DemoLibrary.Models in Program.cs

---
+++ Student/BetterGuestBookApp/ConsoleUI/Program.cs
@@ -0,0 +1,61 @@
+﻿using DemoGuestLibrary.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        public static List<GuestModel> guests = new List<GuestModel>();
+
+        static void Main()
+        {
+            // Get guest info
+            GetGuestsInfo();
+
+            // Print guest info
+            PrintGuestsInfo();
+        }
+
+        static void GetGuestsInfo()
+        {
+            string Continue;
+
+
+            do
+            {
+                GuestModel guest = new GuestModel();
+
+                // Prompt user for name
+                Console.WriteLine();
+                guest.FirstName = GetUserInput("Enter first name: ");
+                guest.LastName = GetUserInput("Enter last name: ");
+                guest.MessageForStaff = GetUserInput("Enter a message for our staff: ");
+                guests.Add(guest);
+
+                Continue = GetUserInput("Are any more guests coming? (yes/no) ");
+            } while (Continue.ToLower() == "yes");
+
+            return;
+        }
+
+        static string GetUserInput(string prompt)
+        {
+            Console.Write($"{prompt}");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+        static void PrintGuestsInfo()
+        {
+            Console.WriteLine();
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+
+            return;
+        }
+    }
+}
---
+++ Student/BetterGuestBookApp/DemoGuestLibrary/Models/GuestModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoGuestLibrary.Models
+{
+    public class GuestModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MessageForStaff { get; set; }
+        public string GuestInfo
+        {
+            get
+            {
+                return $"{FirstName} {LastName} : {MessageForStaff}";
+            }
+        }
+    }
+}
