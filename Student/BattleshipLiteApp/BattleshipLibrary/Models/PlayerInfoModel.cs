79e4d8c43cb97b0e499766859de5cec73d58e152
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-03-03 13:46:07 -0600

    BattleshipLiteApp: Completed assignment to flesh out the methods needed in the ConsoleUI and added a few to the PlayerInfoModel class.

--- Student/BattleshipLiteApp/BattleshipLibrary/Models/PlayerInfoModel.cs
+++ Student/BattleshipLiteApp/BattleshipLibrary/Models/PlayerInfoModel.cs
@@ -9,5 +9,32 @@ namespace BattleshipLibrary.Models
         public List<GridSpotModel> ShotGrid { get; set; }
         public int TotalShots { get; set; }
         public int TotalHits { get; set; }
+
+        // Lib
+        static void InitializeGame()
+        {
+            // Initialize shot grids to all empty status
+            // Initialize all cells to the <row><col> format
+            // Initialize shot count to 0
+            // Initialize hit count to 0
+        }
+
+        // Lib
+        static bool IsValidSpotForShip()
+        {
+            // Check first char is A-E
+            // Check 2nd char is 1-5
+            // Check that not already ship status
+
+        }
+
+        //Lib
+        static bool IsValidSpotForShot()
+        {
+            // Check first char is A-E
+            // Check 2nd char is 1-5
+            // Check that not already ship hit or miss
+
+        }
     }
 }
--- Student/BattleshipLiteApp/ConsoleUI/Program.cs
+++ Student/BattleshipLiteApp/ConsoleUI/Program.cs
@@ -4,6 +4,86 @@
     {
         static void Main(string[] args)
         {
+            // UI
+            WelcomeMessage();
+
+            for (int i = 0; i < 2; i++)
+            {
+                // Lib
+                InitializeGame();
+
+                // UI
+                GetUserName();
+
+
+                // UI
+                GetShipPlacements();
+            }
+
+            do
+            {
+                // UI
+                DisplayGrids();
+
+                // UI
+                GameOver = TakeATurn();
+            } while (!GameOver);
+
+            //UI
+            ShowWinnerAndStats();
+        }
+
+        //UI
+        static void WelcomeMessage()
+        {
+            //Print welcome message to user
+        }
+
+        // UI
+        static void GetUserName()
+        {
+            //Prompt for user name
+        }
+
+        // UI
+        static void GetShipPlacements()
+        {
+            // Is a valid spot?
+            IsValidSpotForShip();
+            // If not, prompt again
+            // If yes, Update ship info list
+            // Set status of cells with ship placed status
+        }
+
+        // UI
+        static void DisplayGrids()
+        {
+            //Go to a specific screen location
+            // Print either the <row><col> or X or O
+        }
+
+        // UI but validity check can be lib
+        static void TakeATurn()
+        {
+            // Get grid location to fire at on opponents grid
+
+
+            // Check if valid location
+            // A..E, 1..5
+            // Not prev missed or hit
+            IsValidSpotForShot();
+            // If miss update cell to miss
+            // If hit update cell to hit
+            //Redraw grids
+            // if 5 hits return GameOver
+
+        }
+
+        // UI
+        static void ShowWinnerAndStats()
+        {
+            // Print {UserName1} or {UserName2} is the winner.
+            // Print number of shots to winn
         }
     }
 }
