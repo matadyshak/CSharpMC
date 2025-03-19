b7bd5f5348e918019347caf7e533c61e75b27fb1
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-03-18 22:42:24 -0500

    New projects for MethodOverrideDemoApp

---
+++ Instructor/MethodOverrideDemoApp/MethodOverrideDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverrideDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = "Tim",
+                LastName = "Corey",
+                Email = "tim@iamtimcorey.com"
+            };
+
+            Console.WriteLine(person);
+
+            Console.ReadLine();
+        }
+    }
+}
