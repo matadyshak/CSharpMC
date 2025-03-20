f2761f42e6b6572c1411d3a80cec1dd9b5c40df1
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-03-19 22:11:33 -0500

    Completed OverrideDemoApp to override the ToString() method for a Person class.

---
+++ Student/OverrideDemoApp/OverrideDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OverrideDempApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person person = new Person
+            {
+                FirstName = "Michael",
+                LastName = "Tadyshak"
+            };
+            Console.WriteLine(person.ToString());
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ Student/OverrideDemoApp/OverrideDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OverrideDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OverrideDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f8bd02f4-a88e-4670-9f67-4ffd5fda885f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
