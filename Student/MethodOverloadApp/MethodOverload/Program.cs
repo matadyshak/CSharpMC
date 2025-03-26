5b841f1c2fde6dce1e2065687b71d20335c34ab2
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-03-25 23:16:54 -0500

    Working MethodOverloadApp.  Created overloaded constructors.

---
+++ Student/MethodOverloadApp/MethodOverload/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main()
+        {
+            EmployeeModel michael = new EmployeeModel("Michael", "Tadyshak", 25.00m, "Customer Service");
+            EmployeeModel myra = new EmployeeModel("Myra", "Tadyshak");
+            EmployeeModel daniel = new EmployeeModel();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class EmployeeModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal HourlyRate { get; set; }
+        public string Department { get; set; }
+
+        public EmployeeModel()
+        {
+        }
+        public EmployeeModel(string firstName, string lastName, decimal hourlyRate, string department)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            HourlyRate = hourlyRate;
+            Department = department;
+        }
+
+        public EmployeeModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            HourlyRate = GetHourlyRate();
+            Department = GetDepartment();
+        }
+
+        public decimal GetHourlyRate()
+        {
+            decimal rate = 0;
+            bool valid = false;
+
+            while (!valid)
+            {
+                Console.Write("Enter hourly rate: ");
+                string input = Console.ReadLine();
+                try
+                {
+                    decimal.TryParse(input, out rate);
+                    valid = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Input: {input} was invalid.  Try again.");
+                }
+            }
+
+            return rate;
+        }
+
+        public string GetDepartment()
+        {
+            string department = "";
+
+            Console.Write("Enter department name: ");
+            department = Console.ReadLine();
+
+            return department;
+        }
+
+    }
+}
