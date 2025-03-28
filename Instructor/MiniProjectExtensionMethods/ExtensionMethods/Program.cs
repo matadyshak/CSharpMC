6dc8c70713f3910493e11bc807676f7cd096e528
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-03-27 22:28:07 -0500

    MiniProjectExtensionMethods - Started by implementing RequestString extension method.

---
+++ Instructor/MiniProjectExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            person.FirstName = "What is your first name: ".RequestString();
+
+            person.LastName = "What is your last name: ".RequestString();
+
+            person.Age = "What is your age: ".RequestInt(0, 120);
+            //person.Age = ConsoleHelper.RequestInt("What is your age: ", 0, 120);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student/MiniProjectExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            person.FirstName = "Enter your first name: ".RequestString();
+            person.LastName = "Enter your last name: ".RequestString();
+            //            person.Age = "Enter your age in years: ".RequestInt();
+
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+        }
+    }
+
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+            bool valid = false;
+
+            while (valid == false)
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(output) == false)
+                {
+                    valid = true;
+                }
+            }
+            return output;
+        }
+    }
+}
+
