18f28bdcc038cf3117b9353f4ce6ac4e49018fbe
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-03-29 22:55:50 -0500

    Working GenericsDemoApp.  Created UIHelperLibrary and brought in previously written PersonModel, AddressModel and ExtensionElemDataTypes for prompting the user for data.

--- Student/GenericsDemoApp/GenericsDemo/Program.cs
+++ Student/GenericsDemoApp/GenericsDemo/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace GenericsDemo
+﻿using System;
+using UIHelperLibrary;
+
+namespace GenericsDemo
 {
     class Program
     {
         static void Main()
         {
+            PersonModel person = new PersonModel();
+            person.FirstName = person.RequestName("Enter your first name: ");
+            person.LastName = person.RequestName("Enter your last name: ");
+
+            AddressModel address = new AddressModel();
+            address.AddressLine1 = address.RequestAddressLine1("Enter Address Line 1: ");
+            address.AddressLine2 = address.RequestAddressLine2("Enter Address Line 2 (or Enter key to skip): ");
+            address.City         = address.RequestCity("Enter City: ");
+            address.State        = address.RequestState("Enter State (Abbreviation Code): ");
+            address.ZipCode      = address.RequestZipCode("Enter ZIP Code: ");
+
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+            Console.WriteLine($"{address.AddressLine1}");
+            Console.WriteLine($"{address.City}, {address.State} {address.ZipCode}");
+
+            string word = "Enter a string: ".RequestString();
+            int integer1 = "Enter an integer value from 0 to 100: ".RequestInt(0, 100);
+            int integer2 = "Enter an integer value: ".RequestInt();
+            double double1 = "Enter a double-precision floating-point value from 0.0 to 4.0: ".RequestDouble(0.0, 4.0);
+            decimal decimal1 = "Enter a decimal value from 0.00 to 1_000_000m: ".RequestDecimal(0.0m, 1_000_000.00m);
+
+            Console.WriteLine($"String: {word}");
+            Console.WriteLine($"Integer 1: {integer1}");
+            Console.WriteLine($"Integer 2: {integer2}");
+            Console.WriteLine($"Double 1: {double1}");
+            Console.WriteLine($"Decimal 1: {decimal1}");
         }
     }
 }
---
+++ Student/GenericsDemoApp/UIHelperLibrary/PersonModel.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace UIHelperLibrary
+{
+    public class PersonModel
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                value = value.Trim();
+                if (string.IsNullOrWhiteSpace(value) || !Regex.IsMatch(value, "^[A-Za-z]+$"))
+                {
+                    throw new ArgumentException("Invalid entry.  Only letters are allowed.");
+                }
+
+                _firstName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value.ToLower());
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                value = value.Trim();
+                if (string.IsNullOrWhiteSpace(value) || !Regex.IsMatch(value, "^[A-Za-z]+$"))
+                {
+                    throw new ArgumentException("Invalid entry.  Only letters are allowed.");
+                }
+
+                _lastName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value.ToLower());
+            }
+        }
+
+        public string RequestName(string prompt)
+        {
+            string entry;
+            string name;
+            // Matches if anything other than letters is found
+            Regex regex = new Regex("[^A-Za-z]+");
+
+            do
+            {
+                Console.Write($"{prompt}");
+                // ? prevents a run-time exception if null is read
+                // If ReadLine returns null the whole expression evaluates to null and .Trim is not called
+                entry = Console.ReadLine();
+                name = entry.Trim();
+
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    name = regex.Replace(name, "");
+                    if (name.Length > 0)
+                    {
+                        return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name.ToLower());
+                    }
+                }
+
+                Console.WriteLine($"Entry: \'{entry}\' is invalid.  Please try again."); ;
+            } while (true);
+        }
+    }
+}
