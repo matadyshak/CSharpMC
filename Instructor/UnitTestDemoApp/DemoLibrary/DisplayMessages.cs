9747db28a4e3e03e41a0803179e0c18b6dccd013
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-04-01 22:47:26 -0500

    Added instructor code for UnittestDemoApp.

---
+++ Instructor/UnitTestDemoApp/DemoLibrary/DisplayMessages.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Red, Green, Refactor
+
+namespace DemoLibrary
+{
+    public class DisplayMessages
+    {
+        public string Greeting(string firstName, int hourOfTheDay)
+        {
+            string output = "";
+            // DataAccess da = new DataAccess();
+            // da.WriteToDB("MyData");
+
+            if (hourOfTheDay < 5)
+            {
+                output = $"Go to bed { firstName }";
+            }
+            else if (hourOfTheDay < 12)
+            {
+                output = $"Good morning { firstName }";
+            }
+            else if (hourOfTheDay < 18)
+            {
+                output = $"Good afternoon { firstName }";
+            }
+            else
+            {
+                output = $"Good evening { firstName }";
+            }
+
+            return output;
+        }
+    }
+}
