645424ef315703f628d7de4d4198e4afef89f472
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-04-03 23:33:18 -0500

    Starting CalculatorUnitTestDemoApp

---
+++ Instructor/CalculatorUnitTestDemoApp/CalculationLibrary/Calculations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculationLibrary
+{
+    public class Calculations
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            double output = 0;
+
+            if (y != 0)
+            {
+                output = x / y;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Student/CalculationUnitTestDemoApp/CalculationLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CalculationLibrary
+{
+    public class Class1
+    {
+
+    }
+}
