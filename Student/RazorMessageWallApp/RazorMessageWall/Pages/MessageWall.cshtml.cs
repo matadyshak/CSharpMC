fd644182bfc245ed293f148ba535726f3a8f4f50
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-04-22 22:06:55 -0500

    RazorMessageWallApp - Put in the code from the video and ran it.

--- Student/RazorMessageWallApp/RazorMessageWall/Pages/MessageWall.cshtml.cs
+++ Student/RazorMessageWallApp/RazorMessageWall/Pages/MessageWall.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace RazorMessageWall.Pages
 {
@@ -8,14 +9,18 @@ namespace RazorMessageWall.Pages
         //BindProperty - When you post data you can set this property
         [BindProperty]
         public string Message { get; set; }
+
+        [BindProperty]
+        public List<string> Messages { get; set; } = new List<string>();
+
         public void OnGet()
         {
         }
 
         public IActionResult OnPost()
         {
+            Messages.Add(Message);
             return Page();  // Will go right back to the same page
-
         }
     }
 }
