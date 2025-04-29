521553a7d5f872a359391df60f199a6f26254e53
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-04-29 09:44:56 -0500

    Working MVCMessageWallApp

--- Student/MVCMessageWallApp/MVCMessageWall/Models/MessageModel.cs
+++ Student/MVCMessageWallApp/MVCMessageWall/Models/MessageModel.cs
@@ -1,7 +1,12 @@
-﻿namespace MVCMessageWall.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMessageWall.Models
 {
     public class MessageModel
     {
+        [Required]
+        [StringLength(10, MinimumLength = 5)]
+        [Display(Name = "Really Cool Message")]
         public string Message { get; set; }
     }
 }
