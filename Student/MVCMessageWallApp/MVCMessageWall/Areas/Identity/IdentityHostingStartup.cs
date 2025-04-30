eba87a9fef65e0896a599063ee75373769865596
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-04-30 09:32:08 -0500

    MVCMessageMapApp is now working

---
+++ Student/MVCMessageWallApp/MVCMessageWall/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVCMessageWall.Areas.Identity.Data;
+using MVCMessageWall.Data;
+
+[assembly: HostingStartup(typeof(MVCMessageWall.Areas.Identity.IdentityHostingStartup))]
+namespace MVCMessageWall.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MVCMessageWallContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MVCMessageWallContextConnection")));
+
+                services.AddDefaultIdentity<MVCMessageWallUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MVCMessageWallContext>();
+            });
+        }
+    }
+}
--- Student/MVCMessageWallApp/MVCMessageWall/Startup.cs
+++ Student/MVCMessageWallApp/MVCMessageWall/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +19,7 @@ namespace MVCMessageWall
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +37,7 @@ namespace MVCMessageWall
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
 
             app.UseRouting();
 
@@ -48,6 +45,8 @@ namespace MVCMessageWall
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
