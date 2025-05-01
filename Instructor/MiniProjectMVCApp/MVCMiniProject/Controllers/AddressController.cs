8db016ec5cd16aaf667597a90a4c126d1ee86d9b
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-01 10:59:29 -0500

    Added instructor version of MiniProjectMVCApp

---
+++ Instructor/MiniProjectMVCApp/MVCMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCMiniProject.Models;
+
+namespace MVCMiniProject.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: Address
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Address/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Address/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AddressModel data)
+        {
+            if (ModelState.IsValid == false)
+            {
+                _logger.LogWarning("The user submitted an invalid Address upon Create.");
+                return View();
+            }
+
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Instructor/MiniProjectMVCApp/MVCMiniProject/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMiniProject.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
