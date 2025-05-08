32b3f28ce1348bac1e9f38754d1da9c6a597aacf
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-07 22:43:47 -0500

    Working BlazorServerMessageWallApp

---
+++ Student/BlazorServerMessageWallApp/BlazorServerMessageWall/Components/Models/MessageModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMessageWall.Components.Models;
+
+public class MessageModel
+{
+    [Required]
+    [StringLength(10, MinimumLength = 5)]
+    //    public string? Message { get; set; }
+    public string Message { get; set; }
+}
