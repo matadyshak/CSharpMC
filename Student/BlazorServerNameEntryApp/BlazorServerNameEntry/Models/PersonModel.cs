51ec600e81c74aafbb9693436f4880667718d5f1
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-11 18:29:48 -0500

    BlazorServerNameEntryApp - Tried entering NameEntry.razor page from memory then made edits to complete the page correctly.

---
+++ Student/BlazorServerNameEntryApp/BlazorServerNameEntry/Models/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerNameEntry.Models;
+public class PersonModel
+{
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    public string FirstName { get; set; }
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    public string LastName { get; set; }
+}
