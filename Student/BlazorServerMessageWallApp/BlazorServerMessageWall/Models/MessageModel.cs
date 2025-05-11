7a885a69c7630d64aa3de47e56871ed35c5f4f78
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-11 14:12:29 -0500

    Removed sample code.  Completed BlazorServerMessageWallApp.

---
+++ Student/BlazorServerMessageWallApp/BlazorServerMessageWall/Models/MessageModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMessageWall.Models;
+
+public class MessageModel
+{
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    public string Message { get; set; }
+}
