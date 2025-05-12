2f4e8a9540b8851930beb5525f12c00dba996835
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-11 21:09:17 -0500

    BlazorServernameAddressEntryApp - Completed data models and razor pages for name and addresses from memory.

---
+++ Student/BlazorServerNameAddressEntryApp/BlazorServerNameAddressEntry/Models/AddressModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerNameAddressEntry.Models
+{
+    public class AddressModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
+        public string State { get; set; }
+        [Required]
+        [StringLength(10, MinimumLength = 5)]
+        public string Zipcode { get; set; }
+    }
+}
---
+++ Student/BlazorServerNameAddressEntryApp/BlazorServerNameAddressEntry/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerNameAddressEntry.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
