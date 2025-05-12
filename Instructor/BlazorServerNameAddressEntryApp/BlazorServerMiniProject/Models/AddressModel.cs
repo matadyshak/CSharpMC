52e4c3cd6a7b2be5ff2d8e5473d7822bba63f796
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-11 19:18:48 -0500

    Copied Instructor code and created student project for BlazorServerNameAddressEntryApp

---
+++ Instructor/BlazorServerNameAddressEntryApp/BlazorServerMiniProject/Models/AddressModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMiniProject.Models;
+
+public class AddressModel
+{
+    [Required]
+    public string StreetAddress { get; set; }
+
+    [Required]
+    public string City { get; set; }
+
+    [Required]
+    [StringLength(2, MinimumLength = 2)]
+    public string State { get; set; }
+
+    [Required]
+    [StringLength(10, MinimumLength = 5)]
+    public string ZipCode { get; set; }
+}
---
+++ Instructor/BlazorServerNameAddressEntryApp/BlazorServerMiniProject/Models/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMiniProject.Models;
+
+public class PersonModel
+{
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+}
