291d612c854f7af400e8c47a5a88df1603ffce38
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-21 20:40:23 -0500

    SimpleWasm completed

--- Student/BlazorWebAssembly2/SimpleWasmApp/SimpleWasm/Models/AddressModel.cs
+++ Student/BlazorWebAssembly2/SimpleWasmApp/SimpleWasm/Models/AddressModel.cs
@@ -1,17 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace SupportedWasm.Shared.Models
+namespace SimpleWasm.Models
 {
     public class AddressModel
     {
         [Required]
         public string AddressLine1 { get; set; }
+
         public string AddressLine2 { get; set; }
+
         [Required]
         public string City { get; set; }
+
         [Required]
         [StringLength(2, MinimumLength = 2)]
         public string State { get; set; }
+
         [Required]
         [StringLength(10, MinimumLength = 5)]
         public string Zipcode { get; set; }
--- Student/BlazorWebAssembly2/SimpleWasmApp/SimpleWasm/Models/PersonModel.cs
+++ Student/BlazorWebAssembly2/SimpleWasmApp/SimpleWasm/Models/PersonModel.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace SupportedWasm.Shared.Models
+namespace SimpleWasm.Models
 {
     public class PersonModel
     {
         [Required]
         public string FirstName { get; set; }
+
         [Required]
         public string LastName { get; set; }
     }
