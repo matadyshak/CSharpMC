cea9143e0d95c2519c264907a582fe8395e957cc
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-21 23:37:23 -0500

    Completed BlazorWebAssembly for SimpleWasm, ProgressiveWasm and SupportedWasm.  The last one not hitting the post commands for some reason.  Going to change to separate lists of names and addresses because current code crashes if there are more people than addresses.

---
+++ Student/BlazorWebAssembly/SupportedWasmApp/SupportedWasm/Server/Controllers/PersonController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupportedWasm.Shared.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SupportedWasm.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/<PersonController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] PersonModel person)
+        {
+            _logger.LogInformation("Posted name: {FirstName} {LastName}",
+                person.FirstName, person.LastName);
+
+        }
+
+        // PUT api/<PersonController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Student/BlazorWebAssembly/SupportedWasmApp/SupportedWasm/Shared/Models/PersonModel.cs
+++ Student/BlazorWebAssembly/SupportedWasmApp/SupportedWasm/Shared/Models/PersonModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace SimpleWasm.Models
+namespace SupportedWasm.Shared.Models
 {
     public class PersonModel
     {
