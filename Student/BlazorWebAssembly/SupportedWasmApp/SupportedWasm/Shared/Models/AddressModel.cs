356ef83c1c5a16963078b8d1961c2b40ea6cf887
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-24 20:16:47 -0500

    SupportedWasmApp - Made AddressLine2 nullable.  Now null is accepted as a valid submission.  Set the server project as the only starting project.  Changed launchSettings.json for client to have "launchBrowser" = false.

--- Student/BlazorWebAssembly/SupportedWasmApp/SupportedWasm/Shared/Models/AddressModel.cs
+++ Student/BlazorWebAssembly/SupportedWasmApp/SupportedWasm/Shared/Models/AddressModel.cs
@@ -7,7 +7,7 @@ namespace SupportedWasm.Shared.Models
         [Required]
         public string AddressLine1 { get; set; }
 
-        public string AddressLine2 { get; set; }
+        public string? AddressLine2 { get; set; }
 
         [Required]
         public string City { get; set; }
