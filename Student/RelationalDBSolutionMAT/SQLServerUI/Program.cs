8ece6030188bf89f73ac3f58798ea49030d42037
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-05-29 16:19:46 -0500

    RelationalDBSolutionMAT - Created new projects and installed NuGet packages for SQL data access, JSON and Dapper.  Added reference to DataAccessLibrary to SQLServerUI

---
+++ Student/RelationalDBSolutionMAT/SQLServerUI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SQLServerUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Processing complete.");
+            Console.ReadLine();
+        }
+    }
+}
