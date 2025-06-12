3e8b65ad456c104f0362d6fd4c06578460339a5d
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-06-12 11:06:53 -0500

    NoSqlDBSolution - Just started.

---
+++ Student/NoSqlDBSolution/MongoDBUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MongoDBUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Done processing MongoDB");
+            Console.ReadLine();
+        }
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string? output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+
+    }
+}
