15b98e7e2214c066b073c2ff9785616b4defb063
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-06-30 22:15:43 -0500

    CosmosDB - Updated to new database on Azure and stored primary key in secrets storage

--- Student/NoSqlDBSolutionCosmos/DataAccessLibrary/Models/ContactModel.cs
+++ Student/NoSqlDBSolutionCosmos/DataAccessLibrary/Models/ContactModel.cs
@@ -11,10 +11,9 @@ namespace DataAccessLibrary.Models
         [BsonElement("_id")]
         [BsonGuidRepresentation(GuidRepresentation.Standard)]
         [JsonProperty(PropertyName = "id")]
-
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public List<EmailAddressModel> EmailAddresses { get; set; } = new List<EmailAddressModel>();
         public List<PhoneNumberModel> PhoneNumbers { get; set; } = new List<PhoneNumberModel>();
     }
