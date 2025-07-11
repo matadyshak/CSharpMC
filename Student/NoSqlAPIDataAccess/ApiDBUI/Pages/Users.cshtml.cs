8c0fdb7557a296244adf979fa8cefca283a9188d
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-07-10 23:01:46 -0500

    NoSqlAPIDataAccess - Added Javascript code as a UI Users page.  Still in debug.

---
+++ Student/NoSqlAPIDataAccess/ApiDBUI/Pages/Users.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class UsersModel : PageModel
+{
+    public void OnGet()
+    {
+        // No server-side logic needed; all handled via JavaScript and API
+    }
+}
