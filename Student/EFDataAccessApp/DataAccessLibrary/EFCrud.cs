05f005ae29b600292c7ec46cf6828dbeb48ded7e
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-07-23 22:17:22 -0500

    EFDataAccessApp - Rearranging the code to isolate database code from the user interface code in a way similar to the first SQL Server project .  In work...

---
+++ Student/EFDataAccessApp/DataAccessLibrary/EFCrud.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLibrary
+{
+    public class EFCrud
+    {
+        private void RemoveUser(int id)
+        {
+            using (var db = new PersonContext())
+            {
+                var user = db.People
+                    .Include(a => a.Addresses)
+                    .Where(p => p.Id == id).First();  // If a C# function is called here EF will download all records
+
+                db.People.Remove(user);
+                db.SaveChanges();
+            }
+        }
+        private void RemovePhoneNumber(int id, string phoneNumber)
+        {
+            using (var db = new PersonContext())
+            {
+                var user = db.Contacts
+                    .Include(p => p.PhoneNumbers)
+                    .Where(c => c.Id == id).First();
+
+                user.PhoneNumbers.RemoveAll(p => p.PhoneNumber == phoneNumber);
+                db.SaveChanges();
+            }
+        }
+        private void UpdateFirstName(int id, string firstName)
+        {
+            using (var db = new PersonContext())
+            {
+                var user = db.Contacts.Where(c => c.Id == id).First();
+
+                user.FirstName = firstName;
+                db.SaveChanges();
+            }
+        }
+
+        private void ReadAll()
+        {
+            using (var db = new PersonContext())
+            {
+                var records = db.Contacts
+                    .Include(e => e.EmailAddresses)
+                    .Include(p => p.PhoneNumbers)
+                    .ToList();
+
+                foreach (var c in records)
+                {
+                    Console.WriteLine($"{c.FirstName} {c.LastName}");
+                }
+            }
+        }
+
+        private void ReadById(int id)
+        {
+            using (var db = new PersonContext())
+            {
+                var user = db.Contacts.Where(c => c.Id == id).First();
+
+                Console.WriteLine($"{user.FirstName} {user.LastName}");
+            }
+        }
+
+    }
+}
--- Student/EFDataAccessApp/EFDataAccess/Program.cs
+++ Student/EFDataAccessApp/EFDataAccess/Program.cs
@@ -1,11 +1,112 @@
-﻿namespace EFDataAccess
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+
+namespace EFDataAccess
 {
-    internal class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
+            EFCrud eFCrud = new EFCrud();
+
+
+            //ReadAllContacts(eFCrud);
+
+            //ReadContact(eFCrud, 3);
+
+            //CreateNewContact(eFCrud);
+
+            //UpdateContact(eFCrud);
+
+            //RemovePhoneNumberFromContact(eFCrud, 1, 1);
+
             Console.WriteLine("Completed processing");
             Console.ReadLine();
         }
+
+        private static void CreatePhil()
+        {
+            var p = new Person
+            {
+                FirstName = "Phil",
+                LastName = "Tady"
+            };
+
+            p.Addresses.Add(new Address
+            {
+                Street = "2154 Fratney St",
+                City = "Milwaukee",
+                State = "WI",
+                ZipCode = "53201"
+            });
+
+            p.Addresses.Add(new Address
+            {
+                Street = "5402 Weil Place",
+                City = "Milwaukee",
+                State = "WI",
+                ZipCode = "53209"
+            });
+
+            p.Employers.Add(new Employer
+            {
+                CompanyName = "SquareD"
+            });
+
+            p.Employers.Add(new Employer
+            {
+                CompanyName = "Pulp Reproduction"
+            });
+
+
+            using (var db = new PersonContext())
+            {
+                db.Contacts.Add(c);
+                db.SaveChanges();
+            }
+        }
+
+        private static void CreateMickey()
+        {
+            var p = new Person
+            {
+                FirstName = "Mickey",
+                LastName = "Toddy"
+            };
+
+            p.Addresses.Add(new Address
+            {
+                Street = "2051 W Kneel St",
+                City = "Milwaukee",
+                State = "WI",
+                ZipCode = "53234"
+            });
+
+            p.Addresses.Add(new Address
+            {
+                Street = "100 W Wisconsin Ave",
+                City = "Milwaukee",
+                State = "WI",
+                ZipCode = "53432"
+            });
+
+            p.Employers.Add(new Employer
+            {
+                CompanyName = "St Michael Hospital"
+            });
+
+            p.Employers.Add(new Employer
+            {
+                CompanyName = "Bobby's Place"
+            });
+
+
+            using (var db = new PersonContext())
+            {
+                db.Person.Add(c);
+                db.SaveChanges();
+            }
+        }
+
     }
 }
