b3c325a6b4b18cdc2bc0182deaa3f2b9242a4a5b
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-08-02 22:34:37 -0500

    HotelApp - Started by creating projects and the SQLHotelDB project and the RoomType table.

---
+++ Student/HotelApp/SQLHotelDB/dbo/Tables/RoomType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[RoomType]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(1024) NOT NULL, 
+    [Price] DECIMAL NOT NULL
+)
