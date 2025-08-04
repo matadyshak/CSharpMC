b27598878e98b8b0495f2e4b2841fe0809097815
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-08-04 17:20:14 -0500

    HotelApp - Completed database table definitions and script to populate test data for Rooms table and RoomTypes table.  Published the DB.  This is before renaming the library from DataAccessLibrary to HotelAppLibrary

--- Student/HotelApp/HotelAppDB/dbo/Tables/RoomTypes.sql
+++ Student/HotelApp/HotelAppDB/dbo/Tables/RoomTypes.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [dbo].[RoomType]
+﻿CREATE TABLE [dbo].[RoomTypes]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Title] NVARCHAR(50) NOT NULL, 
     [Description] NVARCHAR(2000) NOT NULL, 
     [Price] MONEY NOT NULL
---
+++ Student/HotelApp/HotelAppDB/dbo/Tables/Rooms.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Rooms]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RoomTypeId] INT NOT NULL, 
+    [RoomNumber] VARCHAR(10) NOT NULL, 
+    CONSTRAINT [FK_Rooms_RoomTypes] FOREIGN KEY (RoomTypeId) REFERENCES RoomTypes(Id) 
+)
