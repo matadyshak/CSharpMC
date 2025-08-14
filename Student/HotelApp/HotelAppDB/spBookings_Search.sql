1303d136f8e1e4fa5359627780ae2aaf5b8fdb17
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-08-14 11:11:39 -0500

    HotelAPP - Branch HotelApp-SearchReservations.  Working on the stored procedure.

---
+++ Student/HotelApp/HotelAppDB/spBookings_Search.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Search]
+	@lastName NVARCHAR(50),
+	@dateToday date
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	DECLARE @guestId int;
+
+	SELECT *
+	from dbo.Bookings b
+	WHERE b.LastName = @lastName AND b.StartDate <= @dateToday AND b.EndDate >= @dateToday;
+	SELECT @guestId = Id
+	FROM dbo.Guests 
+	WHERE LastName = @lastName;
+
+
+
+END;
