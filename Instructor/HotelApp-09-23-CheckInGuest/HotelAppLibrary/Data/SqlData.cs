eeddb41b44001f43afe1da392aeda238829da7e2
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-08-18 22:09:35 -0500

    HotelApp - Master - Deleted data model BookingModel.cs.  This was added while trying the Search for Bookings homework but is superceded by the BookingFullModel class.  Added instructror code for CheckInGuest.

--- Instructor/HotelApp-09-23-CheckInGuest/HotelAppLibrary/Data/SqlData.cs
+++ Instructor/HotelApp-09-23-CheckInGuest/HotelAppLibrary/Data/SqlData.cs
@@ -1,5 +1,9 @@
 ﻿using HotelAppLibrary.Databases;
 using HotelAppLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace HotelAppLibrary.Data
 {
@@ -12,46 +16,46 @@ namespace HotelAppLibrary.Data
         {
             _db = db;
         }
+
         public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
         {
             return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",
-                                                        // same name in sp and this method: camel case
-                                                        new { startDate, endDate },
-                                                        connectionStringName,
-                                                        true);
+                                                 new { startDate, endDate },
+                                                 connectionStringName,
+                                                 true);
         }
 
         public void BookGuest(string firstName,
-                               string lastName,
-                               DateTime startDate,
-                               DateTime endDate,
-                               int roomTypeId)
+                              string lastName,
+                              DateTime startDate,
+                              DateTime endDate,
+                              int roomTypeId)
         {
-            GuestModel guest = _db.LoadData<GuestModel, dynamic>("spGuests_Insert",
+            GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert",
                                                                  new { firstName, lastName },
                                                                  connectionStringName,
                                                                  true).First();
 
-            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("SELECT * FROM dbo.RoomTypes WHERE Id = @Id",
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id",
                                                                           new { Id = roomTypeId },
                                                                           connectionStringName,
                                                                           false).First();
 
             TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
 
-            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("spRooms_GetAvailableRooms",
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
                                                                               new { startDate, endDate, roomTypeId },
                                                                               connectionStringName,
                                                                               true);
 
             _db.SaveData("dbo.spBookings_Insert",
-                         new
-                         {
-                             roomId = availableRooms.First().Id,
+                         new 
+                         { 
+                             roomId = availableRooms.First().Id, 
                              guestId = guest.Id,
                              startDate = startDate,
                              endDate = endDate,
-                             totalCost = roomType.Price * timeStaying.Days
+                             totalCost = timeStaying.Days * roomType.Price
                          },
                          connectionStringName,
                          true);
@@ -60,9 +64,14 @@ namespace HotelAppLibrary.Data
         public List<BookingFullModel> SearchBookings(string lastName)
         {
             return _db.LoadData<BookingFullModel, dynamic>("dbo.spBookings_Search",
-                                              new { lastName, startDate = DateTime.Now.Date },
-                                              connectionStringName,
-                                              true);
+                                                    new { lastName, startDate = DateTime.Now.Date },
+                                                    connectionStringName,
+                                                    true);
+        }
+
+        public void CheckInGuest(int bookingId)
+        {
+            _db.SaveData("dbo.spBookings_CheckIn", new { Id = bookingId }, connectionStringName, true);
         }
     }
 }
