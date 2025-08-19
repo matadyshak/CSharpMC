2d2b3bbdffd909dc09e3afa208cd639dda8cf10a
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-08-18 19:55:02 -0500

    HotelAPP - master - completed changes per video on searching for reservations

---
+++ Student/HotelApp/HotelAppDB/dbo/Stored Procedures/spBookings_Search.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Search]
+	@lastName NVARCHAR(50),
+	@startDate date
+AS
+BEGIN
+    SET NOCOUNT ON;
+	SELECT [b].[Id], [b].[RoomId], [b].[GuestId], [b].[StartDate], [b].[EndDate], 
+	    [b].[CheckedIn], [b].[TotalCost], [g].[FirstName], [g].[LastName],
+		[r].[RoomTypeId], [r].[RoomNumber], [rt].[Title], [rt].[Description],
+		[rt].[Price]
+	FROM dbo.Bookings b
+	INNER JOIN dbo.Guests g ON b.GuestId = g.Id
+	INNER JOIN dbo.Rooms r ON b.RoomId = r.Id
+	INNER JOIN dbo.RoomTypes rt ON r.RoomTypeId = rt.Id
+	WHERE b.StartDate = @startDate AND g.LastName = @lastName;
+END;
