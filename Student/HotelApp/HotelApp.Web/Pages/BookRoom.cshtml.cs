08259f2f726648b5e7222f03a888311bb028743b
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-09-18 23:46:49 -0500

    HotelApp Master - Changed date format to "MMM dd, yyyy".  Renamed RoomType property to RoomTypeModel.  Added validation for FirstName & LastName as annotations and in OnPost(). Problem with RoomTypeId becoming 0 and room type displaying as 0 and URL params converted to display %2F chars, etc.

--- Student/HotelApp/HotelApp.Web/Pages/BookRoom.cshtml.cs
+++ Student/HotelApp/HotelApp.Web/Pages/BookRoom.cshtml.cs
@@ -2,6 +2,7 @@ using HotelAppLibrary.Data;
 using HotelAppLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelApp.Web.Pages
 {
@@ -9,20 +10,25 @@ namespace HotelApp.Web.Pages
     {
         private readonly IDatabaseData _db;
 
-        [BindProperty(SupportsGet = true)]
-        public int RoomTypeId { get; set; }
-        public RoomTypeModel RoomTypeModel { get; set; }
-
+        [DataType(DataType.Date)]
         [BindProperty(SupportsGet = true)]
         public DateTime StartDate { get; set; }
 
+        [DataType(DataType.Date)]
         [BindProperty(SupportsGet = true)]
         public DateTime EndDate { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public int RoomTypeId { get; set; }
+
+        public RoomTypeModel RoomTypeModel { get; set; }
+
         [BindProperty]
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
 
         [BindProperty]
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
 
         public BookRoomModel(IDatabaseData db)
@@ -44,7 +50,21 @@ namespace HotelApp.Web.Pages
 
         public IActionResult OnPost()
         {
-            _db.BookGuest(FirstName, LastName, StartDate, EndDate, RoomTypeId);
+            RoomTypeModel = _db.GetRoomTypeById(RoomTypeId);
+
+            // Validate form data before booking
+            if (string.IsNullOrWhiteSpace(FirstName) || string.IsNullOrWhiteSpace(LastName))
+            {
+                ModelState.AddModelError(string.Empty, "First name and last name are required.");
+                return Page();
+            }
+
+            _db.BookGuest(FirstName,
+                          LastName,
+                          StartDate,
+                          EndDate,
+                          RoomTypeId);
+
             return RedirectToPage("/Index");
         }
     }
