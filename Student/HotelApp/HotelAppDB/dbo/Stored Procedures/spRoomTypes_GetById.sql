c1568320930c98476295b834dba32f74b7fa73cd
matadyshak <161066263+matadyshak@users.noreply.github.com>
2025-09-24 21:03:09 -0500

    HotelApp-BookRoom - Stored Procedure spRoomTypes_GetById was missing from the project.  Copied that in as a new sp and published.  Now GetAvailableRoomTypes and BookRoom are both working.

---
+++ Student/HotelApp/HotelAppDB/dbo/Stored Procedures/spRoomTypes_GetById.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spRoomTypes_GetById]
+	@Id int
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	SELECT [Id], [Title], [Description], [Price]
+	FROM [dbo].[RoomTypes]
+	WHERE [Id] = @Id;
+END
